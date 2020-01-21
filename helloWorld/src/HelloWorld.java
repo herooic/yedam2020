6977d3d151488f9d842c112e668f15e139788e30
User <User@YD02-17>
2020-01-21 12:37:21 +0900

    hl

---
+++ helloWorld/src/HelloWorld.java
@@ -0,0 +1,22 @@
+
+public class HelloWorld {
+	/* 
+	여러라인을 주석처리할때 사용합니다.
+	작성자 : 이현우 
+	*/
+	public static void main(String[] args) {
+		// Hellow World 출력하는 구문.
+		// age 에 10을 담겠다라는 의미 
+		int age = 31;
+		int year = 2020;
+		System.out.println("올해는 " + year + " 년");
+		System.out.println("나이는 " + age + " 입니다");
+		//  (+) 구문만 넣을시 합이 나옴
+	
+		// Sysout 컨트롤 스페이스 누르면 자동완성
+		// 컨트롤  f11 실행 버튼
+		
+		
+		
+	}  
+}
